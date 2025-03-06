1aabd112fceb7bdd399fecab498ca710dd6fb828
qrutyy <qrutyq@gmail.com>
2025-03-06 13:00:02 +0300

    feat: add more OS-side opt checks + improve fio

--- test/perf.sh
+++ test/perf.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+VBD_NAME="lsvbd1"
 BD_NAME="$1"
 FLAMEGRAPH_PATH="./FlameGraph"
 BLOCK_DEVICE_PATH="/sys/block/${BD_NAME:-vdb}/queue/scheduler"
+IO_DEPTH=16
+
+prioritise_fio()
+{
+	echo -e "\nPrioritise fio process..."
+	for pid in $(pidof fio); do
+		echo -e "\nchrt $pid"
+		chrt -r 99 "$pid"
+	done
+}
 
 if [ ! command -v perf &> /dev/null ]; then
     echo "Error: 'perf' is not installed. Please install it and try again."
@@ -22,6 +33,13 @@ if [[ ! -f "$FLAMEGRAPH_PATH/stackcollapse-perf.pl" || ! -f "$FLAMEGRAPH_PATH/fl
     echo "FlameGraph successfully cloned."
 fi
 
+###								###
+###			CONFIG PART			###
+###								###
+
+echo -e "\nBlock device info"
+sudo lsblk -o NAME,MAJ:MIN,SIZE,TYPE,FSTYPE,SCHED,ROTA,PHY-SEC,TYPE,MIN-IO,OPT-IO | grep $BD_NAME
+
 echo -e "\nChecking symbols for lsvbd.ko..."
 sudo cat /proc/kallsyms | grep lsbdd
 
@@ -33,14 +51,45 @@ echo -e "\nLower kernel restrictions"
 sudo sysctl kernel.kptr_restrict=0
 sudo sysctl kernel.perf_event_paranoid=1
 
+echo -e "\nCheck queue depth"
+if [ "$(cat /sys/block/$BD_NAME/queue/nr_requests)" -le "$IO_DEPTH" ]; then
+	echo "$IO_DEPTH" > /sys/block/$BD_NAME/queue/nr_requests
+fi
+
+echo -e "\nCheck if CONFIG_BLOCK is enabled"
+if [ "$(zgrep CONFIG_BLOCK= /boot/config-$(uname -r))" != "CONFIG_BLOCK=y" ]; then
+	echo -e "\nError: Recompile the kernel with CONFIG_BLOCK=y"
+	exit -1 
+fi
+
+echo -e "\nCheck CPU governors"
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
+echo -e "\nDisble iostats"
+#echo 0 > /sys/block/$BD_NAME/queue/iostats 
+#echo 0 > /sys/block/$VBD_NAME/queue/iostats 
+
+echo -e "\nDisable merges"
+echo 2 > /sys/block/$BD_NAME/queue/nomerges
+echo 2 > /sys/block/$VBD_NAME/queue/nomerges
+
+
+###								  ###
+###		PERFORMANCE TEST PART	  ###
+###								  ###
+
 echo -e "\nAdding probes for lsvbd.ko functions..."
 sudo perf probe -x ../src/lsbdd.ko --add '*(*)' || echo "Warning: Some probes may already exist."
 
 echo -e "\nStarting fio write workload..."
-sudo perf record -g -F 99 -a -o perf_write.data -- make fio_perf_w ID=16 NJ=4
+sudo perf record -g -F 99 -a -o perf_write.data -- make fio_perf_w_opt ID=$IO_DEPTH NJ=4 
+prioritise_fio
+
+# & makes fio_perf run in the background, so the prioritise_fio can actually find the fio process
 
 echo -e "\nStarting fio read workload..."
-sudo perf record -g -F 99 -a -o perf_read.data -- make fio_perf_r ID=16 NJ=4
+sudo perf record -g -F 99 -a -o perf_read.data -- make fio_perf_r_opt ID=$IO_DEPTH NJ=4 
+prioritise_fio
 
 if [ ! -s perf_write.data ] || [ ! -s perf_read.data ]; then
     echo "Error: perf results are empty. Profiling may have failed."
@@ -52,8 +101,10 @@ fi
 #sudo perf script > lsbdd-trace.txt
 
 echo -e "\nGenerating FlameGraph... (lsbdd only)"
+
 perf script -f -i perf_write.data | $FLAMEGRAPH_PATH/stackcollapse-perf.pl | grep lsbdd > out_write.folded
 sudo $FLAMEGRAPH_PATH/flamegraph.pl --width 2500 --height 16 out_write.folded > lsbdd_fg_spec_write.svg
+
 perf script -f -i perf_read.data | $FLAMEGRAPH_PATH/stackcollapse-perf.pl | grep lsbdd > out_read.folded
 sudo $FLAMEGRAPH_PATH/flamegraph.pl --width 2500 --height 16 out_read.folded > lsbdd_fg_spec_read.svg
 
@@ -73,3 +124,4 @@ fi
 #fi
 
 echo -e "\nFlameGraph successfully generated: lsbdd_fg_spec_read.svg and lsbdd_fg_spec_write.svg"
+
