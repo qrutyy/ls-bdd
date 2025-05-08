cbd794ce050199e78042208cacce46e92ffad9e2
qrutyy <qrutyq@gmail.com>
2025-05-08 14:54:30 +0300

    feat: move atomic macro's in sep file

---
+++ src/utils/lock-free/atomic_ops.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#ifndef ATOMIC_OPS_H
+#define ATOMIC_OPS_H
+
+// SYNC GCC SPECIFIC
+
+/** 
+ * Can be set as gcc function __sync_val_compare_and_swap, both of them support atomicity so basically it doesn't matter (atomic-gcc.h even has a macro that makes it equal)
+ * atomic_* versions of cmpxchg or other actions are used for better cross-architecture work. Btw, they provide more memory order guarantees and type safety.
+ */
+
+#define SYNC_LCAS(ptr, old, new) cmpxchg64(ptr, old, (__typeof__(*ptr))new)
+#define SYNC_LSWAP(ptr, val) xchg(ptr, val)
+
+#define ATOMIC_LREAD(ptr) atomic64_read(ptr)
+#define ATOMIC_INC(ptr) atomic64_inc(ptr)
+#define ATOMIC_FAI(ptr) atomic64_fetch_add(1, ptr)
+#define ATOMIC_FAD(ptr) atomic64_fetch_sub(1, ptr)
+#define ATOMIC_LCAS(ptr, old, new) atomic64_cmpxchg(ptr, old, new)
+#define ATOMIC_LSWAP(ptr, val) atomic64_xchg(ptr, val)
+
+#endif
---
+++ src/utils/lock-free/marked_pointers.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#ifndef MARKED_POINTERS_H
+#define MARKED_POINTERS_H
+
+// TAGS SPECIFIC
+
+#define TAG_VALUE(v, tag) ((v) | tag)
+#define IS_TAGGED(v, tag) ((v) & tag)
+
+// removes the tag from the pointer
+#define STRIP_TAG(v, tag) (((size_t)v) & ~tag)
+
+// marks the node by switching the last bit to 1 and vice versa
+#define MARK_NODE(x) TAG_VALUE((size_t)(x), 0x1)
+// returns bool* if pointer is marked
+#define HAS_MARK(x) (IS_TAGGED(((size_t)x), 0x1) == 0x1)
+
+#endif
